f2593e0b141d7fe499cced95b329799caf99ec99
Yee Lin Tan <yeelin@uw.edu>
2015-10-15 17:45:44 -0700

    Added a second fragment to show when the user is logged in. Activity handles the swapping in and out of fragments. The second fragment is pretty simple for now - it only shows the user's name, profile picture and an option to log out.

--- app/src/main/java/com/example/yeelin/projects/betweenus/activity/LoginActivity.java
+++ app/src/main/java/com/example/yeelin/projects/betweenus/activity/LoginActivity.java
@@ -3,28 +3,176 @@ package com.example.yeelin.projects.betweenus.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
 import com.example.yeelin.projects.betweenus.R;
+import com.example.yeelin.projects.betweenus.fragment.LoginFragment;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 
 /**
  * Created by ninjakiki on 10/13/15.
  */
 public class LoginActivity
-        extends BaseActivity {
+        extends BaseActivity
+        implements LoginFragment.SkipLoginCallback {
 
+    //logcat
     private static final String TAG = LoginActivity.class.getCanonicalName();
+    //constants
+    private static final int LOGIN = 0;
+    private static final int LOGOUT = 1;
+    private static final int FRAGMENT_COUNT = LOGOUT + 1;
 
+    //member variables
+    private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+    private boolean userSkippedLogin = false;
+    private boolean isResumed = false;
+
+    //facebook-related
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+
+    /**
+     * Builds the appropriate intent for starting this activity
+     * @param context
+     * @return
+     */
     public static Intent buildIntent(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
         return intent;
     }
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //init view and toolbar
         setContentView(R.layout.activity_login);
-        setupToolbar(R.id.login_toolbar, false);
+        setupToolbar(R.id.login_toolbar, true);
 
+        //init fb sdk
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
 
+        //init access token tracker
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (isResumed) {
+                    FragmentManager fm = getSupportFragmentManager();
+                    int backStackSize = fm.getBackStackEntryCount();
+                    for (int i=0; i<backStackSize; i++) {
+                        fm.popBackStack();
+                    }
+
+                    //check if new state means user is logged in
+                    if (currentAccessToken != null) {
+                        //yes, user is logged in
+                        Log.d(TAG, "onCurrentAccessTokenChanged: User is now logged in");
+                        showFragment(LOGOUT, false);
+                    }
+                    else {
+                        //no, user is not logged in
+                        Log.d(TAG, "onCurrentAccessTokenChanged: User is no longer logged in");
+                        showFragment(LOGIN, false);
+                    }
+                }
+            }
+        };
+
+        //store references to both fragments
+        FragmentManager fm = getSupportFragmentManager();
+        fragments[LOGIN] = fm.findFragmentById(R.id.login_fragment);
+        fragments[LOGOUT] = fm.findFragmentById(R.id.logout_fragment);
+
+        //hide both fragments
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.hide(fragments[LOGIN])
+                .hide(fragments[LOGOUT])
+                .commit();
     }
+
+    @Override
+    public void onSkipLoginClicked() {
+        userSkippedLogin = true;
+        navigateUpToParentActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isResumed = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isResumed = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+    }
+
+    private void showLoginFragment() {
+        showFragment(LOGIN, true);
+    }
+
+    private void showLogoutFragment() {
+        showFragment(LOGOUT, true);
+    }
+
+    private void showFragment(int fragmentIndex, boolean addToBackStack) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        for (int i=0; i<fragments.length; i++) {
+            if (i == fragmentIndex) transaction.show(fragments[i]);
+            else transaction.hide(fragments[i]);
+        }
+        if (addToBackStack) {
+            transaction.addToBackStack(null); //no name for the transaction
+        }
+        transaction.commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+
+        //check if user is already logged in
+        if (AccessToken.getCurrentAccessToken() != null) {
+            //yes, user is logged in
+            Log.d(TAG, "onResumeFragments: User is logged in");
+            showFragment(LOGOUT, false);
+        }
+        else {
+            //no, user is not logged in
+            Log.d(TAG, "onResumeFragments: User is not logged in");
+            showFragment(LOGIN, false);
+        }
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Log.d(TAG, String.format("onActivityResult: Request code:%s, Result code:%d: ", requestCode, resultCode));
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        callbackManager.onActivityResult(requestCode, resultCode, data);
+//    }
 }
--- app/src/main/java/com/example/yeelin/projects/betweenus/fragment/LoginFragment.java
+++ app/src/main/java/com/example/yeelin/projects/betweenus/fragment/LoginFragment.java
@@ -1,8 +1,12 @@
 package com.example.yeelin.projects.betweenus.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,66 +22,136 @@ import com.facebook.login.widget.LoginButton;
 /**
  * Created by ninjakiki on 10/13/15.
  */
-public class LoginFragment
-        extends Fragment
-        implements FacebookCallback<LoginResult>, View.OnClickListener {
+public class LoginFragment extends Fragment {
+
+    //logcat
+    private static final String TAG = LoginFragment.class.getCanonicalName();
+    //member variables
     private SkipLoginCallback skipLoginCallback;
     private CallbackManager callbackManager;
+
+    /**
+     * Callback from this fragment for whoever who is interested when the user skips fb login
+     */
+    public interface SkipLoginCallback {
+        void onSkipLoginClicked();
+    }
+
+    /**
+     * Make sure the activity or parent fragment implements the callback.
+     * @param context
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        Object objectToCast = getParentFragment() != null ? getParentFragment() : getActivity();
+        try {
+            skipLoginCallback = (SkipLoginCallback) objectToCast;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(objectToCast.getClass().getSimpleName() + " must implement SkipLoginCallback");
+        }
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fb_login, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_fb_login, container, false);
 
         //create the callback manager
         callbackManager = CallbackManager.Factory.create();
 
-        ViewHolder viewHolder = new ViewHolder(view);
+        //setup view holder
+        final ViewHolder viewHolder = new ViewHolder(view);
         view.setTag(viewHolder);
 
         //setup the login button
         viewHolder.loginButton.setReadPermissions("user_friends");
         viewHolder.loginButton.setFragment(this);
-        viewHolder.loginButton.registerCallback(callbackManager, this);
+        viewHolder.loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onCancel: Login success.");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel: Login canceled.");
+
+                //create a snackbar to inform the user
+                if (viewHolder != null) {
+                    final Snackbar snackbar = Snackbar.make(viewHolder.rootView, "Oops! Facebook login error.", Snackbar.LENGTH_LONG);
+                }
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError: Login error. " + error.getLocalizedMessage());
+
+                //create a snackbar to inform the user
+                if (viewHolder != null) {
+                    final Snackbar snackbar = Snackbar.make(viewHolder.rootView, "Oops! Facebook login error.", Snackbar.LENGTH_LONG);
+                }
+            }
+        });
 
         //setup the skip login button
-        viewHolder.skipLoginButton.setOnClickListener(this);
+        viewHolder.skipLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (skipLoginCallback != null) {
+                    skipLoginCallback.onSkipLoginClicked();
+                }
+            }
+        });
+
+        return view;
     }
 
+    /**
+     * Nullify the listener
+     */
     @Override
-    public void onSuccess(LoginResult loginResult) {
-
+    public void onDetach() {
+        skipLoginCallback = null;
+        super.onDetach();
     }
 
+    /**
+     * This is called when the FB login activity returns.  We are required to pass the result to callback manager.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
-    public void onCancel() {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, String.format("onActivityResult: Request code:%s, Result code:%d: ", requestCode, resultCode));
+        super.onActivityResult(requestCode, resultCode, data);
 
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    @Override
-    public void onError(FacebookException error) {
-
+    /**
+     * Return the view's view holder if it exists
+     * @return
+     */
+    private ViewHolder getViewHolder() {
+        View view = getView();
+        return view != null ? (ViewHolder) view.getTag() : null;
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    public interface SkipLoginCallback {
-        void onSkipLoginClicked();
-    }
-
+    /**
+     * View holder class
+     */
     private class ViewHolder {
+        final View rootView;
         final LoginButton loginButton;
         final TextView skipLoginButton;
 
         ViewHolder(View view) {
+            rootView = view.findViewById(R.id.root_layout);
             loginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
             skipLoginButton = (TextView) view.findViewById(R.id.skip_fb_login_button);
         }
